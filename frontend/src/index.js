cf0d198f8f61cc6f025324ee98bd76d0d9a204cd
Manikandan151 <manikmanikandan1510@gmail.com>
2024-02-12 22:03:41 +0530

    updated

---
+++ frontend/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { configureChains, WagmiConfig, createClient } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { sepolia } from '@wagmi/chains';
+import './index.css'
+try{
+  const { provider, webSocketProvider } = configureChains(
+    [sepolia],
+    [publicProvider()],
+  )
+  
+  const client = createClient({
+    autoConnect: true,
+    provider,
+    webSocketProvider,
+  })
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
+  );
+}
+catch(e){
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+      <App />
+  );
+  console.log(e);
+} 
